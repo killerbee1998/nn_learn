20a7f99f7d21414d8ae191d1cd3c4e69ab340739
riasat1998 <riasat1998@gmail.com>
2020-10-11 00:16:28 +0600

    import train test split

--- Ann/artificial_neural_network.py
+++ Ann/artificial_neural_network.py
@@ -1,6 +1,7 @@
 # import libs
 import pandas as pd
 import numpy as np
+from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import OneHotEncoder, LabelEncoder
 from sklearn.compose import ColumnTransformer 
 
