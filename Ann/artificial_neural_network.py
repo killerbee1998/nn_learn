554099e6d8d07865eaaacdbc700a0e956b5e3dc2
riasat1998 <riasat1998@gmail.com>
2020-10-11 00:02:16 +0600

    import encoders

--- Ann/artificial_neural_network.py
+++ Ann/artificial_neural_network.py
@@ -1,9 +1,10 @@
 # import libs
 import pandas as pd
 import numpy as np
-
+from sklearn.preprocessing import OneHotEncoder, LabelEncoder
 
 #import dataset and select x,y
 dataset = pd.read_csv("Churn_Modelling.csv")
 X = dataset.iloc[:, 3:13].values
 y = dataset.iloc[:, 13].values
+
