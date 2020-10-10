d5b3989303267cd013c2fa1e5658cc443c9ae32b
riasat1998 <riasat1998@gmail.com>
2020-10-11 00:00:38 +0600

    import dataset and select x,y

--- Ann/artificial_neural_network.py
+++ Ann/artificial_neural_network.py
@@ -1,3 +1,9 @@
 # import libs
 import pandas as pd
 import numpy as np
+
+
+#import dataset and select x,y
+dataset = pd.read_csv("Churn_Modelling.csv")
+X = dataset.iloc[:, 3:13].values
+y = dataset.iloc[:, 13].values
