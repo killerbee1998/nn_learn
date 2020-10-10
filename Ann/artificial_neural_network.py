94cac3cfe88ef6bfbe410744d08ecbf26ae65bff
riasat1998 <riasat1998@gmail.com>
2020-10-11 00:05:58 +0600

    encode country and gender

--- Ann/artificial_neural_network.py
+++ Ann/artificial_neural_network.py
@@ -8,3 +8,8 @@ dataset = pd.read_csv("Churn_Modelling.csv")
 X = dataset.iloc[:, 3:13].values
 y = dataset.iloc[:, 13].values
 
+# labelEncode country and gender
+labelEncoder_X1 = LabelEncoder()
+X[:, 1] = labelEncoder_X1.fit_transform(X[:, 1])
+labelEncoder_X2 = LabelEncoder()
+X[:, 2] = labelEncoder_X2.fit_transform(X[:, 2])
