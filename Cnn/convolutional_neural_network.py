baace52b8c365ab93abc05f4fafcef3146649446
riasat1998 <riasat1998@gmail.com>
2020-10-12 22:01:18 +0600

    start cnn with conv2d layer

--- Cnn/convolutional_neural_network.py
+++ Cnn/convolutional_neural_network.py
@@ -2,3 +2,6 @@
 from keras.models import Sequential
 from keras.layers import Dense, Conv2D, MaxPool2D, Flatten
 
+# cnn layers
+cnn = Sequential()
+cnn.add(Conv2D(filters=64, kernel_size=3, activation='relu', input_shape=[64,64,3]))
