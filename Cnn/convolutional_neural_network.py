86a0d59262eac842f2ffdf09834b1f0f6d3a6b36
riasat1998 <riasat1998@gmail.com>
2020-10-12 22:23:16 +0600

    test cnn

--- Cnn/convolutional_neural_network.py
+++ Cnn/convolutional_neural_network.py
@@ -1,7 +1,9 @@
 # import libs
+import numpy as np
 from keras.models import Sequential
 from keras.layers import Dense, Conv2D, MaxPool2D, Flatten
 from keras.preprocessing import image_dataset_from_directory
+from keras.preprocessing.image import img_to_array, load_img
 
 # load images
 train_set = image_dataset_from_directory('dataset/training_set',
@@ -20,6 +22,19 @@ cnn.add(Flatten())
 cnn.add(Dense(units=128, activation='relu'))
 cnn.add(Dense(units=1, activation='sigmoid'))
 
-# compile cnn
+# compile cnnload_img('dataset/single_prediction/cat_or_dog_1.jpg', target_size=(64,64))
 cnn.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 cnn.fit(x=train_set, validation_data=test_set, epochs=25)
+
+# test cnn
+test_img1 = load_img('dataset/single_prediction/cat_or_dog_1.jpg', target_size=(64,64))
+test_img1 = img_to_array(test_img1)
+test_img1 = np.expand_dims(test_img1, axis = 0)
+res1 = cnn.predict(test_img1)
+print(res1)
+
+test_img2 = load_img('dataset/single_prediction/cat_or_dog_2.jpg', target_size=(64,64))
+test_img2 = img_to_array(test_img2)
+test_img2 = np.expand_dims(test_img2, axis = 0)
+res2 = cnn.predict(test_img2)
+print(res2)
