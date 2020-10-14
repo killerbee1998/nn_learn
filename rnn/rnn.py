9257b7a8fbc177c41a4639f5ae33e3961b073e10
riasat1998 <riasat1998@gmail.com>
2020-10-14 21:00:07 +0600

    normalized training set

--- rnn/rnn.py
+++ rnn/rnn.py
@@ -2,6 +2,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
 
 # import datasets
 train_set = pd.read_csv("Google_Stock_Price_Train.csv")
@@ -9,3 +10,7 @@ train_ara = train_set.iloc[:, 1:2].values
 
 test_set = pd.read_csv("Google_Stock_Price_Test.csv")
 test_ara = test_set.iloc[:, 1:2].values
+
+# feature scaling
+sc = MinMaxScaler(feature_range=(0,1))
+train_ara_scaled = sc.fit_transform(train_ara)
