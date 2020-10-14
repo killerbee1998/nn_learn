96d455f1c9db5fa2c0a6d3a295549bbeb3d684bf
riasat1998 <riasat1998@gmail.com>
2020-10-14 21:22:05 +0600

    minor refactoring

--- rnn/rnn.py
+++ rnn/rnn.py
@@ -16,11 +16,15 @@ sc = MinMaxScaler(feature_range=(0,1))
 train_ara_scaled = sc.fit_transform(train_ara)
 
 # create data structure with timesteps
+# create data structure with timesteps
 x_train = []
 y_train = []
-for i in range(60, 1258):
-    x_train.append(train_ara_scaled[i-60:i, 0])
-    y_train.append(train_ara_scaled[i, 0])
+for i in range(0, 1198):
+    x_train.append(train_ara_scaled[i:i+60, 0])
+    y_train.append(train_ara_scaled[i+60, 0])
 
 x_train = np.array(x_train)
 y_train = np.array(y_train)
+
+# reshape
+x_train = np.reshape(x_train, (1198,), )
