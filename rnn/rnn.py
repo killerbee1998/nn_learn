8c1ef68647f1f5cd91d14df52f5edc02cdb51750
riasat1998 <riasat1998@gmail.com>
2020-10-14 20:50:47 +0600

    convert dfs to np arrays

--- rnn/rnn.py
+++ rnn/rnn.py
@@ -4,5 +4,8 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # import datasets
-train_set = pd.read_csv("Goofle_Stock_Price_Train.csv")
-test_set = pd.read_csv("Goofle_Stock_Price_Test.csv")
+train_set = pd.read_csv("Google_Stock_Price_Train.csv")
+train_ara = train_set.iloc[:, 1:2].values
+
+test_set = pd.read_csv("Google_Stock_Price_Test.csv")
+test_ara = test_set.iloc[:, 1:2].values
