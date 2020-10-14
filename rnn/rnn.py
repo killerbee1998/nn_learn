27b84f079c3c3a75b9e1cb3ede44f22e3ae503c8
riasat1998 <riasat1998@gmail.com>
2020-10-14 20:43:00 +0600

    import datasets as dataframes

--- rnn/rnn.py
+++ rnn/rnn.py
@@ -2,3 +2,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+
+# import datasets
+train_set = pd.read_csv("Goofle_Stock_Price_Train.csv")
+test_set = pd.read_csv("Goofle_Stock_Price_Test.csv")
